e5fbe01fc1d0296c8e83a221e7b1447b8650d1e5
Joel Bennett <Jaykul@HuddledMasses.org>
2021-03-07 14:08:18 -0500

    Update run_this and readme

--- README.md
+++ README.md
@@ -1,50 +1,79 @@
 # opsdroid skill random
 
-A skill for [opsdroid](https://github.com/opsdroid/opsdroid) to perform random events like flipping a coin or rolling a dice.
+A skill for [opsdroid](https://github.com/opsdroid/opsdroid) to run code in docker containers.
 
 ## Requirements
 
-None.
+1. Opsdroid must have access to run docker.
+2. You must have a docker volume configured which opsdroid and the scripts can use for scratch files.
+
+### Docker
+
+If you're running Opsdroid in a docker container, you'll have to pass the docker .sock to it in order to for this to work.
+
+In order to use docker with opsdroid _from inside_ docker, you'll want to create an image with the docker cli in it:
+
+1. Make a `Dockerfile` and add these lines:
+
+```
+FROM opsdroid/opsdroid:latest
+RUN apk update && apk add --no-cache docker-cli
+```
+
+2. Build the image:
+
+```
+docker build -t opsdroiddocker .
+```
+
+3. Create a volume:
+```
+docker volume create working
+```
+
+4. Run Opsdroid with the volume mounted, and pass in the docker.sock. On linux:
+```
+docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -v working:/code opsdroiddocker
+```
 
 ## Configuration
 
-None.
+- `volume`. The volume mount to use with the containers we create
+- `path`. The path where opsdroid can save files to have them show up in that volume
+- `containers`. One or more containers which must each have:
+    - `language`. Strings which users can use after code fences for this language
+    - `extension`. A file extension to use for scripts in this language
+    - `container`. The name of the container (optionally including the repository)
+    - `command`. The command to run in the docker container
+
+```yaml
+  docker:
+    repo: https://github.com/Jaykul/skill-random.git
+    volume: working:/code
+    path: /code
+    containers:
+      - language: ["pwsh","powershell"]
+        extension: ".ps1"
+        container: "mcr.microsoft.com/powershell"
+        command: ['pwsh','-NoLogo','-NonInteractive','-NoProfile','-File']
+      - language: ["python","py"]
+        extension: ".py"
+        container: "python"
+        command: ['python']
+```
+
+
+The docker skill will write code to a file, mount that file in the specified container, and then run the specified command ...
 
 ## Usage
 
-#### `flip a coin`
+### Run this:
 
-Flips a coin.
-
-> user: flip a coin
->
-> opsdroid: It landed on heads
-
-#### `roll a die`
-
-Rolls a die.
-
-> user: roll a die
->
-> opsdroid: You rolled a 4
-
-#### `roll a d20`
-
-Does full DnD-style dice rolls:
-
-> user: roll 3d8+2
->
-> opsdroid: You rolled 3d8 (4, 2, 2) + 2 = 10
-
-Even supports "keep highest" (or lowest), minimums, and re-rolls (see [d20](https://github.com/avrae/d20)).
-> user: roll 3d20kh1
->
-> opsdroid: You rolled 3d20kh1 (15, 14, 11) = 15
-
-### Magic 8 Ball
-
-Picks a random clairvoyant style answer:
-
-> user: magic 8 ball, should I go to bed now?
->
-> opsdroid: Without a doubt
+>     user: run this:
+>           ```pwsh
+>           "Jim", "Bob", "Mark" | Get-Random
+>           ```
+>     opsdroid: Let me try that in `mcr.microsoft.com/powershell`
+>     opsdroid: ```
+>               Jim
+>               ```
--- __init__.py
+++ __init__.py
@@ -1,110 +1,120 @@
+from opsdroid.skill import Skill
 from opsdroid.matchers import match_regex
-from voluptuous import Required
+
+from voluptuous import Required, All, Length, Range
 from tempfile import TemporaryDirectory, NamedTemporaryFile
-import docker, asyncio, re, os, traceback, subprocess
-
+import asyncio, re, os, traceback, subprocess
 CONFIG_SCHEMA = {
-    Required("containers", {
-        "PowerShell": {
-            "names": ("pwsh","powershell"),
-            "extension": ".ps1",
-            "container": "mcr.microsoft.com/powershell",
-            "command": ['pwsh','-NoLogo','-NonInteractive','-NoProfile','-File']
-        },
-        "Python": {
-            "names": ("python","py"),
-            "extension": ".py",
-            "container": "python",
-            "command": ['python']
-        }
-    }): dict
+    Required("volume"): All(str, Length(min=1)),
+    Required("workdir"): All(str, Length(min=2)),
+    Required("containers"): list
+        # All({
+        #     Required("language"): All(str),
+        #     Required("extension"): str,
+        #     Required("container"): str,
+        #     Required("command"): All(str)
+        # })
 }
+# , default = [
+#         {
+#             "language": ["pwsh","powershell"],
+#             "extension": ".ps1",
+#             "container": "mcr.microsoft.com/powershell",
+#             "command": ['pwsh','-NoLogo','-NonInteractive','-NoProfile','-File']
+#         },
+#         {
+#             "language": ["python","py"],
+#             "extension": ".py",
+#             "container": "python",
+#             "command": ['python']
+#         }
+#     ]
+class Docker(Skill):
+    # import docker, re
+    # CLIENT = docker.from_env()
+    # result = CLIENT.containers.run(
+    #     container,
+    #     #language["command"].format('/code/{}'.format(filename)),
+    #     "ls /code",
+    #     auto_remove = True,
+    #     volumes = {'working': { 'bind': '/code', 'mode':'ro'}}
+    #     )
+    # result = result.decode("utf8")
+    # result = re.compile(r'\\n').sub("<br/>\n",result)
 
-CLIENT = docker.DockerClient(base_url='unix:///var/run/docker.sock')
-# CLIENT = docker.from_env()
+    @match_regex(r'(?m)run this:.*[\r\n]+```(?P<lang>.+)[\r\n]+(?P<code>(.*\n?)+)[\r\n]+```', case_sensitive=False)
+    async def run_this(self, opsdroid, config, message):
+        languageCode = message.regex.group('lang')
+        language = next((lang for lang in config["containers"] if languageCode in lang["language"]), None)
 
-@match_regex(r'(?m)run this:.*[\r\n]+```(?P<lang>.+)[\r\n]+(?P<code>(.*\n?)+)[\r\n]+```', case_sensitive=False)
-async def run_this(opsdroid, config, message):
-    lang = message.regex.group('lang')
-    language = [language for language in config["containers"].values() if(lang in language["names"])]
+        if (language == None):
+            await message.respond(f"Sorry, I don't know {languageCode}")
+            return
 
-    if (language == None):
-        await message.respond(f"Sorry, I don't know {lang}")
-        return
+        container = language["container"]
+        code = message.regex.group('code')
 
-    language = language[0]
-    container = language["container"]
-    code = message.regex.group('code')
+        if (code == None or len(code) == 0):
+            await message.respond(f"Wait, run what?")
+            return
 
-    if (code == None or len(code) == 0):
-        await message.respond(f"Wait, run what?")
-        return
+        await message.respond(f"<p>Let me try that in <code>{container}</code></p>")
 
-    await message.respond(f"<p>Let me try that in <code>{container}</code></p>")
-    # This requires you to have a mounted /working volume
-    # with TemporaryDirectory(dir='/working') as temp:
-    codefile = NamedTemporaryFile(mode='w+t', suffix=language["extension"], dir='/working', delete=False)
-    codefile.writelines(code)
-    codefile.close()
-    head, filename = os.path.split(codefile.name)
-    print("Container name: {}".format(container))
-    # print("Working directory: {}".format(temp))
-    print("File name: {}".format(codefile.name))
-    print("Command: {} /code/{}".format(" ".join(language["command"]), filename))
+        # This requires you to have a mounted volume
+        codefile = NamedTemporaryFile(mode='w+t', suffix=language["extension"], dir=config["workdir"], delete=False)
+        codefile.writelines(code)
+        codefile.close()
+        volume, workdir = config["volume"].split(":")
+        filename = "{}/{}".format(workdir, os.path.split(codefile.name)[1])
 
-    try:
-        # This requires you to have "working" volume you can mount
-        process = subprocess.run(
-            ['docker', 'run', '-v', 'working:/code', container] + language["command"] + [f'/code/{filename}'],
-            capture_output=True,
-            encoding="UTF8")
-        # result = CLIENT.containers.run(
-        #     container,
-        #     #language["command"].format('/code/{}'.format(filename)),
-        #     "ls /code",
-        #     auto_remove = True,
-        #     volumes = {'working': { 'bind': '/code', 'mode':'ro'}}
-        #     )
-        # result = result.decode("utf8")
-        # result = re.compile(r'\\n').sub("<br/>\n",result)
-        if (process.returncode == 0):
-            await message.respond("<pre>{}</pre>".format(process.stdout))
-        else:
-            print("Command exited with {}".format(process.returncode))
-            print(process.stderr)
+        try:
+            # This requires you to have "working" volume you can mount
+            process = subprocess.run(
+                ['docker', 'run', '-v', config["volume"], container] + language["command"] + [filename],
+                capture_output=True,
+                encoding="UTF8")
 
-    except docker.errors.ContainerError:
-        await message.respond("An error occurred. Sorry, but there's no error logging yet.")
-        traceback.print_exc()
+            if (process.returncode == 0):
+                if (process.stderr):
+                    await message.respond("<pre><b>ERROR:</b> {}</pre>".format(process.stderr))
+                if (process.stdout):
+                    await message.respond("<pre>{}</pre>".format(process.stdout))
+            else:
+                print("Command exited with {}".format(process.returncode))
+                print(process.stderr)
 
-# class Message:
+        except docker.errors.ContainerError:
+            await message.respond("An error occurred. Sorry, but there's no error logging yet.")
+            traceback.print_exc()
 
-#     def __init__(self, text):
-#         import re
-#         rr = re.compile(r'(?m)run this:.*[\r\n]+```(?P<lang>.*)[\r\n]+(?P<code>(.*\n)+)```', re.IGNORECASE)
-#         self.text = text
-#         print("Message: \n  pattern: {}\n  text: {}".format(rr, text))
-#         self.regex = rr.match(text)
+    # class Message:
 
-#     async def respond(self, response):
-#         print(str(response))
+    #     def __init__(self, text):
+    #         import re
+    #         rr = re.compile(r'(?m)run this:.*[\r\n]+```(?P<lang>.*)[\r\n]+(?P<code>(.*\n)+)```', re.IGNORECASE)
+    #         self.text = text
+    #         print("Message: \n  pattern: {}\n  text: {}".format(rr, text))
+    #         self.regex = rr.match(text)
 
-# class Config:
-#     def __init__(self, dict):
-#         self.containers = dict
+    #     async def respond(self, response):
+    #         print(str(response))
+
+    # class Config:
+    #     def __init__(self, dict):
+    #         self.containers = dict
 
 
-# if __name__ == "__main__":
-#     import sys
-#     message = Message(sys.argv[1])
-#     asyncio.run(run_this(
-#         None,
-#         Config({
-#             "PowerShell": {
-#                 "names": ("pwsh","powershell"),
-#                 "extension": ".ps1",
-#                 "container": "mcr.microsoft.com/powershell",
-#                 "command": 'pwsh -NoLogo -NonInteractive -NoProfile -File {}'
-#             }
-#         }),
-#         message))
+    # if __name__ == "__main__":
+    #     import sys
+    #     message = Message(sys.argv[1])
+    #     asyncio.run(run_this(
+    #         None,
+    #         Config({
+    #             "PowerShell": {
+    #                 "languages": ("pwsh","powershell"),
+    #                 "extension": ".ps1",
+    #                 "container": "mcr.microsoft.com/powershell",
+    #                 "command": 'pwsh -NoLogo -NonInteractive -NoProfile -File {}'
+    #             }
+    #         }),
+    #         message))
